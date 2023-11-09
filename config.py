35c5b0923bd2014a4c70cdc4e278bd150bc65b78
Yi Xian <exian0716@gmail.com>
2023-11-09 23:29:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-ROBOT_HOST = '169.254.6.28'
+#ROBOT_HOST = '169.254.6.28'
+ROBOT_HOST = '192.168.189.129'
 ROBOT_PORT = 30004
-#config_filename = 'config/main-config.xml'
-config_filename = 'config/control_loop_configuration.xml'
+config_filename = 'config/main-config.xml'
+#config_filename = 'config/control_loop_configuration.xml'
