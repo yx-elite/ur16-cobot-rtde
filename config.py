8fcc46da44d7fa0d6a438f9fc67a6529b012ccce
Yi Xian <exian0716@gmail.com>
2023-11-09 22:15:07 +0800

    Regular code update

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-ROBOT_HOST = '192.168.189.129'
+ROBOT_HOST = '169.254.6.28'
 ROBOT_PORT = 30004
-config_filename = 'config/main-config.xml'
+#config_filename = 'config/main-config.xml'
+config_filename = 'config/control_loop_configuration.xml'
